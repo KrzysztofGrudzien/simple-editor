6c5ae95db05a0d481571ab1fd481aa9b01afdc4a
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-07-18 00:01:04 +0200

    feat: add functions increase and decrease font-size

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 // "use strict";
 const btnReload = document.querySelector('.message__btn--reload-js');
 const btnAdd = document.querySelector('.form__btn--js');
+const btnSizeDown = document.querySelector('.editor__btn--size-down-js');
+const btnSizeUp = document.querySelector('.editor__btn--size-up-js');
 const textMessage = document.querySelector('.form__textarea--js');
+const allTextMessages = document.querySelector('.editor__textarea-edit--js');
 
 const messages = [];
+let size = 16;
 
 function addMessage(e) {
     e.preventDefault();
@@ -11,8 +15,17 @@ function addMessage(e) {
     textMessage.value = "";
     console.log(messages.length);
 }
+console.log(messages);
 
-console.log(messages)
+function fontSizeDown() {
+    allTextMessages.style.fontSize = `${size - 1}px`;
+    size = size - 1;
+}
+
+function fontSizeUp() {
+    allTextMessages.style.fontSize = `${size + 1}px`;
+    size = size + 1;
+}
 
 function reloadServer() {
     location.reload(true);
@@ -20,3 +33,5 @@ function reloadServer() {
 
 btnReload.addEventListener('click', reloadServer);
 btnAdd.addEventListener('click', addMessage);
+btnSizeDown.addEventListener('click', fontSizeDown);
+btnSizeUp.addEventListener('click', fontSizeUp);
