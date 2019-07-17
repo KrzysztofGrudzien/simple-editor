35b2865ca3c01accbe59f8621c7b0d37c867f48a
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-07-17 23:36:11 +0200

    feat: add function adding messages to the array

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,22 @@
-"use strict";
+// "use strict";
 const btnReload = document.querySelector('.message__btn--reload-js');
+const btnAdd = document.querySelector('.form__btn--js');
+const textMessage = document.querySelector('.form__textarea--js');
+
+const messages = [];
+
+function addMessage(e) {
+    e.preventDefault();
+    messages.push(textMessage.value);
+    textMessage.value = "";
+    console.log(messages.length);
+}
+
+console.log(messages)
 
 function reloadServer() {
     location.reload(true);
 }
 
 btnReload.addEventListener('click', reloadServer);
+btnAdd.addEventListener('click', addMessage);
